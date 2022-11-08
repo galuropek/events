5af92f3f24ac2c3f50fbc40d01bc8f55207b12c2
galuropek <galuropek@gmail.com>
2022-11-08 22:17:07 +0300

    Add tests to Event model and controoller

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :development, :test do
   gem 'rspec-rails', '~> 6.0.0'
   gem 'factory_bot_rails'
   gem 'rails-controller-testing'
+  gem 'rubocop'
 end
 
 group :development do
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,50 @@
 class EventsController < ApplicationController
+  before_action :load_event, only: %i[show edit update destroy]
+
   def index
     @events = Event.all
+  end
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def edit
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path
+  end
+
+  private
+
+  def load_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :description)
   end
 end
