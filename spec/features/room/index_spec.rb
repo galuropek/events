3202d3d81dd32a162a2ebb6b0495b4cfb85e6a79
galuropek <galuropek@gmail.com>
2023-03-20 00:03:47 +0300

    add feature test: User can view a list of available rooms

---
+++ spec/features/room/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'User can view a list of created room', %q{
+  In order to view all available rooms
+  I'd like to be able to visit page with rooms list
+} do
+
+  given!(:rooms) { create_list(:room, 3) }
+
+  scenario 'User views a rooms list' do
+    visit rooms_path
+
+    expect(page).to have_content rooms[0].title
+    expect(page).to have_content rooms[1].title
+    expect(page).to have_content rooms[2].title
+  end
+end
